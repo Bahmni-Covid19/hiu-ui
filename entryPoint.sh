3c4e0702dea1ce8a5ee89d40cd8e085fadcb8dc5
SanoferSameera <sanofersameera25@gmail.com>
2022-03-18 15:55:19 +0530

    BAH-1444 | Refactor. extracted path in script file
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+
+file="/dist/bundle.js"
 set -xe
 : "${BACKEND_BASE_URL?BACKEND_BASE_URL is not defined}"
 : "${BASE_NAME?BASE_NAME is not defined}"
 
-sed -i 's|REACT_APP_SITE_TITLE:[^,]*|REACT_APP_SITE_TITLE: '\"$TITLE\"'|g' /dist/bundle.js
-sed -i 's|ENV_BACKEND_BASE_URL:[^,]*|ENV_BACKEND_BASE_URL: '\"$BACKEND_BASE_URL\"'|g' /dist/bundle.js
-sed -i 's|ENV_BACKEND_API_PATH:[^,]*|ENV_BACKEND_API_PATH: '\"$BACKEND_API_PATH\"'|g' /dist/bundle.js
-sed -i 's|ENV_BASE_NAME:[^,]*|ENV_BASE_NAME: '\"$BASE_NAME\"'|g' /dist/bundle.js
-sed -i 's|ENV_DICOM_VIEWER_PAGE:[^,]*|ENV_DICOM_VIEWER_PAGE: '\"$DICOM_VIEWER_PAGE\"'|g' /dist/bundle.js
-sed -i 's|ENV_DICOM_SERVER_PATH:[^,]*|ENV_DICOM_SERVER_PATH: '\"$DICOM_SERVER_PATH\"'|g' /dist/bundle.js
-sed -i 's|ENV_TIMEZONE_OFFSET:[^,]*|ENV_TIMEZONE_OFFSET: '\"$TIMEZONE_OFFSET\"'|g' /dist/bundle.js
+sed -i 's|REACT_APP_SITE_TITLE:[^,]*|REACT_APP_SITE_TITLE: '\"$TITLE\"'|g' $file
+sed -i 's|ENV_BACKEND_BASE_URL:[^,]*|ENV_BACKEND_BASE_URL: '\"$BACKEND_BASE_URL\"'|g' $file
+sed -i 's|ENV_BACKEND_API_PATH:[^,]*|ENV_BACKEND_API_PATH: '\"$BACKEND_API_PATH\"'|g' $file
+sed -i 's|ENV_BASE_NAME:[^,]*|ENV_BASE_NAME: '\"$BASE_NAME\"'|g' $file
+sed -i 's|ENV_DICOM_VIEWER_PAGE:[^,]*|ENV_DICOM_VIEWER_PAGE: '\"$DICOM_VIEWER_PAGE\"'|g' $file
+sed -i 's|ENV_DICOM_SERVER_PATH:[^,]*|ENV_DICOM_SERVER_PATH: '\"$DICOM_SERVER_PATH\"'|g' $file
+sed -i 's|ENV_TIMEZONE_OFFSET:[^,]*|ENV_TIMEZONE_OFFSET: '\"$TIMEZONE_OFFSET\"'|g' $file
 
 
 exec "$@"
--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@ const Config = {
     REACT_APP_SITE_TITLE: 'NCG',
     ENV_BACKEND_BASE_URL: 'https://ndhm-dev.bahmni-covid19.in',
     ENV_BACKEND_API_PATH: '/hiu-api',
-    ENV_BASE_NAME: '@',
+    ENV_BASE_NAME: '/hiu',
     ENV_DICOM_VIEWER_PAGE: '/viewer/',
     ENV_DICOM_SERVER_PATH: '/',
     ENV_TIMEZONE_OFFSET: '+05:30',
